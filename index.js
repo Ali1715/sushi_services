1cabac6454226be7c1595c056d32e1b5b6216ca9
Ali1715 <tacoomaturanoalison@gmail.com>
2024-02-21 12:33:12 -0400

    delete inde.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// Importar los módulos necesarios
-const express = require('express');
-
-// Crear una instancia de Express
-const app = require('./src/app');
-
-app.listen(app.get('port'), () => {
-  console.log(`Servidor escuchando en el puerto`, app.get('port'));
-});
